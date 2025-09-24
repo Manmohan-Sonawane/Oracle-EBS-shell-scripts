a885c8df15270619e72c30d20b10df6b7971de0b
Manmohan-Sonawane <129599687+Manmohan-Sonawane@users.noreply.github.com>
2025-09-24 17:39:39 +0530

    Create test.sql
    to test the env

---
+++ test.sql
@@ -0,0 +1,7 @@
+ask Name   : TASK_1631154
+Tuning Task Owner  : APPS
+Workload Type      : Single SQL Statement
+Scope              : COMPREHENSIVE
+Time Limit(seconds): 1800
+Completion Status  : INTERRUPTED
+Started at         : 09/24/2025 12:4
